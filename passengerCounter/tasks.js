f1a43341404f500333571c39591b7b9333d56a09
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-04 20:58:59 +0300

    first commit

---
+++ passengerCounter/tasks.js
@@ -0,0 +1,50 @@
+// First task
+// myAge variable
+
+// let myAge = 23;
+// console.log(myAge);
+
+// Second task
+// humanDogRation variable
+
+// let humanDogRation = 7; // 1 human year ≈ 7 dog years
+// myDogAge calculate
+// let myDogAge = myAge * humanDogRation;
+// console.log(myDogAge);
+
+// Third task
+// initialize the bonusPoints with 50
+// let bonusPoints = 50;
+// increase bonusPoints to 100 then to 25 last to 70
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45;
+// console.log(bonusPoints);
+
+// Fourth task
+// function prints 42
+// let print42 = () => console.log(42);
+// print42();
+
+// Fifth task
+// log the sum of laps time
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// let timeSum = () => console.log(lap1 + lap2 + lap3);
+// timeSum();
+
+// Sixth task
+// count the laps done
+// let lapsCompleted = 0;
+
+// let doneLap = () => lapsCompleted++;
+
+// doneLap();
+// doneLap();
+// doneLap();
+
+// console.log(lapsCompleted);
