90e7481ae0963181f717e1f6f95633fdeec02e59
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-05 19:41:03 +0300

    adding increment and save functions and buttons

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,2 +1,16 @@
-let number = 1;
-let increse = () => console.log(number++);
+// initialize the counter with 0
+let number = 0;
+// get the html counter element
+let counterElement = document.getElementById('counter');
+
+// function: increse the counter interacting with the increment botton
+let increse = () => {
+    number = number + 1;
+    counterElement.innerHTML = number;
+};
+
+// function: logs the counter
+let save = () => {
+    console.log(number);
+};
+
