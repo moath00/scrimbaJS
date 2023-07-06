aad7030e06ba8ab6f55a49c80da2a699aa702d2d
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-06 19:11:37 +0300

    adding final touch on passenger counter project.

--- passengerCounter/index.js
+++ passengerCounter/index.js
@@ -1,9 +1,13 @@
 // initialize the counter with 0
 let number = 0;
+// initialize the sum with 0
+let summation = 0;
 // get the html counter element
 let counterElement = document.getElementById('count');
 // get the html save element
 let saveElement = document.getElementById('save-dis');
+// get the html sum element
+let sumElement = document.getElementById('sum');
 
 // function: increse the counter interacting with the increment botton
 let increse = () => {
@@ -13,11 +17,16 @@ let increse = () => {
 
 // function: logs the counter
 let save = () => {
-    if (saveElement.innerText === 'Previouse entered :') {
-        saveElement.innerText += ' ' + number;
+    let text = '';
+    if (saveElement.textContent === 'Previouse entered :') {
+        text += ' ' + number;
+    } else if ((saveElement.innerHTML.length % 25 >= 2 && saveElement.innerHTML.length % 25 <= 6)) {
+        text += '<br>' + number;
     } else {
-        saveElement.innerText += ' - ' + number;
+        text += ' - ' + number;
     }
+    saveElement.innerHTML += text;
+    sum();
     reset();
 };
 
@@ -26,4 +35,10 @@ let save = () => {
 let reset = () => {
     number = 0;
     counterElement.innerHTML = number;
+};
+
+// function: sum the people
+let sum = () => {
+    summation += number;
+    sumElement.innerHTML = "sum : " + summation;
 };
