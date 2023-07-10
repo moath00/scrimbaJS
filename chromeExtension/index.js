d8e513443aef70b3b81fe431aab6726af8c68a84
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-10 10:39:37 +0300

    Added the save tap freature, solve some tasks.

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -1,23 +1,17 @@
 const inputElement = document.querySelector("#input-el");
 const saveElement = document.querySelector("#input-btn");
+const tabSaveElement = document.querySelector("#tab-btn")
+const deleteElement = document.querySelector("#delete-btn");
 const ulElement = document.querySelector("#ul-el");
 
-let myLeads = [];
-
-saveElement.addEventListener("click", () => {
-    let inputText = inputElement.value;
-    myLeads.push(inputText);
-    renderLeads();
-});
-
-let renderLeads = () => {
+let render = (leads) => {
     let htmlData = "";
     ulElement.innerHTML = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         htmlData += `
         <li>
-            <a href='https://${myLeads[i]}' target='_blank'>
-                ${myLeads[i]}
+            <a href='https://${leads[i]}' target='_blank'>
+                ${leads[i]}
             </a>
         </li>
     `;
@@ -27,10 +21,76 @@ let renderLeads = () => {
     inputElement.value = "";
 };
 
-// we can use another way from line 13 to get the same result
+let myLeads = [];
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+];
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+
+saveElement.addEventListener("click", () => {
+    let inputText = inputElement.value;
+    myLeads.unshift(inputText);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    // console.log(localStorage.getItem("myLeads"));
+    render(myLeads);
+});
+
+tabSaveElement.addEventListener("click", () => {
+    myLeads.unshift(inputElement.value);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
+deleteElement.addEventListener("dblclick", () => {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+});
+
+
+// Sixth task
+// let myLeads = `["www.awesomelead.com"]`
+// myLeads = JSON.parse(myLeads);
+// myLeads.push("www.youtube.com");
+
+// myLeads = JSON.stringify(myLeads);
+// console.log(typeof myLeads);
+
+// Fifth task
+// localStorage.setItem("My name","Moath Hjjawi");
+// const name = localStorage.getItem("My name");
+// console.log(name);
+// localStorage.clear();
+
+
+// ########################################## ************ ########################################## //
+// let htmlData += "<li>" + myLead[i] + "</li>";
+// we can use another way from previous line to get the same result
 // create element
 // set text content
 // append to ul
-// const li = document.createElement("li")
-// li.textContent = myLeads[i]
-// ulEl.append(li)
+// const li = document.createElement("li");
+// li.textContent = myLeads[i];
+// ulEl.append(li);
+
+// truly values
+// [] and any value except the falsy mentioned
+
+// falsy values
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+// NaN
+
+// variables vs arguments vs parameters
+// variables are values outside of function and its call
+// arguments are values or variables passed to function call, outside of function too
+// parameters are values used in function, inside of function, its signature
--- chromeExtension/tasks/index.js
+++ chromeExtension/tasks/index.js
@@ -16,12 +16,86 @@
 // ################### ***** ################### //
 
 
-// Third task
+// Third task && Forth task
 // template strings/literals
 
-const recipient = "James";
+// const recipient = "James";
+// const sender = "Moath Hjjawi";
 
-// Refactor the email string to use template strings
-const email = `Hey ${recipient}! How is it going? Cheers Per`;
+// // Refactor the email string to use template strings
+// const email = `Hey ${recipient}!
+// How is it going?
+// Cheers ${sender}`;
 
-console.log(email);
+// console.log(email);
+
+// Seventh task
+// console.log(Boolean("")) // false (empty string)
+// console.log(Boolean("0")) // true (not empty string)
+// console.log(Boolean(100)) // true (number : not null & not zero)
+// console.log(Boolean(null)) // false (null == undefined)
+// console.log(Boolean([0])) // true (array : not empty array)
+// console.log(Boolean(-0)) // false (zero)
+// truly values
+// [] and any value except the falsy mentioned
+
+// falsy values
+// false // 0 // "" // NaN
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+
+
+// ################### ***** ################### //
+
+
+// Eighth task
+// create function take greeting parameter
+// const welcomeEl = document.getElementById("welcome-el");
+
+// // Give the function a parameter, greeting, that replaces "Welcome back"
+// function greetUser(greeting) {
+//     welcomeEl.textContent = greeting + ", Per Harald Borgen 👋";
+// }
+
+// greetUser("Hola");
+
+
+// ################### ***** ################### //
+
+
+// Ninth task
+// create function take greeting, name and emojis that replace using string literals
+// const welcomeEl = document.getElementById("welcome-el");
+
+// // Give the function a parameter, greeting, that replaces "Welcome back"
+// function greetUser(greeting, name, emoji) {
+//     welcomeEl.textContent = `${greeting}, ${name} ${emoji}`;
+// }
+
+// greetUser("Hola", "Moath", "👋");
+
+
+// ################### ***** ################### //
+
+
+// Tenth task
+// create a function add two numbers
+// let add = (number1, number2) => number1 + number2;
+
+// console.log(add(3, 4));
+// console.log(add(9,102))
+
+
+// ################### ***** ################### //
+
+
+// Eleventh task
+// create a function returns first item in array
+let testArray1 = [1, 2, 3, 4];
+let testArray2 = ["a", "b", "c", "d"];
+let testArray3 = [true, false]
+let getFirst = (array) => array[0];
+
+console.log(getFirst(testArray1));
+console.log(getFirst(testArray2));
+console.log(getFirst(testArray3));
