c849ab318d548c968e734cef2bd40a71b59f1cd1
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-10 23:52:32 +0300

    Adding more files with fireDatabase and some tasks.

--- mobileApp/main/index.js
+++ mobileApp/main/index.js
@@ -1,8 +1,27 @@
-// Challenge:
-// Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSetting = {
+  databaseURL : 'https://realtime-database-b1fc9-default-rtdb.asia-southeast1.firebasedatabase.app/'
+};
+
+const app = initializeApp(appSetting);
+const database = getDatabase(app);
+const shoppingListDB = ref(database, "shoppingList");
+
 const inputField = document.querySelector('#input-field');
 const addBtn = document.querySelector('#add-button');
+const list = document.querySelector('#shopping-list');
 
+let clearInputField = (field) => field.value = '';
+let addHTMLContent = (tag, addedTag, value) => {
+  tag.innerHTML += `<${addedTag}>${value}</${addedTag}>`;
+};
+
+// refactor the code clear input field and the code modify the html contents
 addBtn.addEventListener('click', () => {
-  console.log(inputField.value);
+  let inputValue = inputField.value;
+  push(shoppingListDB, inputValue);
+  clearInputField(inputField);
+  addHTMLContent(list, li, inputValue);
 });
