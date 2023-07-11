b91acefb95123a905c89663485c49c7e6babaaa4
MBP <mbp@MBPs-MacBook-Pro.local>
2023-07-11 22:48:04 +0300

    Added final touch and complete the app, it ready to deploy.

--- mobileApp/main/index.js
+++ mobileApp/main/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, onValue, push, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSetting = {
   databaseURL : 'https://realtime-database-b1fc9-default-rtdb.asia-southeast1.firebasedatabase.app/'
@@ -13,9 +13,23 @@ const inputField = document.querySelector('#input-field');
 const addBtn = document.querySelector('#add-button');
 const list = document.querySelector('#shopping-list');
 
+const listHTML = list.innerHTML;
+
 let clearInputField = (field) => field.value = '';
-let addHTMLContent = (tag, addedTag, value) => {
-  tag.innerHTML += `<${addedTag}>${value}</${addedTag}>`;
+let clearHTMLContent = (tag) => tag.innerHTML = '';
+let addHTMLContent = (tagToAddedIn, addedTag, item) => {
+  let itemID = item[0];
+  let itemName = item[1];
+  // tag.innerHTML += `<${addedTag}>${value}</${addedTag}>`;
+
+  let newElement = document.createElement(addedTag);
+
+  newElement.addEventListener('click', () => {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`);
+    remove(exactLocationOfItemInDB);
+  });
+  newElement.textContent = itemName;
+  tagToAddedIn.append(newElement);
 };
 
 // refactor the code clear input field and the code modify the html contents
@@ -23,5 +37,20 @@ addBtn.addEventListener('click', () => {
   let inputValue = inputField.value;
   push(shoppingListDB, inputValue);
   clearInputField(inputField);
-  addHTMLContent(list, li, inputValue);
+});
+
+// Challenge: Change the onValue code so that it uses snapshot.exists() to show items when there are items in the database and if there are not displays the text 'No items here... yet'.
+onValue(shoppingListDB, (snapshot) => {
+  if (snapshot.exists()) {
+    let shoppingListArray = Object.entries(snapshot.val());
+    
+    clearHTMLContent(list);
+    for (let i = 0; i < shoppingListArray.length; i++) {
+        let item = shoppingListArray[i];
+        addHTMLContent(list, "li", item);
+    }
+  }
+  else {
+    list.innerHTML = "No items here... yet";
+  }
 });
